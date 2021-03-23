efff685366548779df96f0d1888f2ba724275786
tangxue <11183504@qq.com>
2021-03-23 21:11:18 +0800

    update

--- docs/mianshi/JS/index.js
+++ docs/mianshi/JS/index.js
@@ -7,7 +7,7 @@ function Person() {
  * prototype 是函数才会有的属性
  */
 
-Person.prototype.name = 'Kevin';
+Person.prototype.name = 'Kevin'
 
 // 实例，构造函数的实例
 const p1 = new Person()
@@ -25,38 +25,37 @@ const p2 = new Person()
 
 
 async function async1() {
-  console.log('2async1 start');
+  console.log('2async1 start')
   await async2()
-  console.log('async1 end');
+  console.log('async1 end')
   
 }
 
 async function async2() {
   new Promise(function (resolve) {
-    console.log('3primuse1');
+    console.log('3primuse1')
     resolve()
   }).then(function () {
-    console.log('6primuse2');
+    console.log('6primuse2')
   })
 }
 
-console.log('1scritp start');
+console.log('1scritp start')
 
 setTimeout(function () {
-  console.log('setTimeout');
+  console.log('setTimeout')
 }, 0)
 
 async1()
 
 new Promise(function (resolve) {
-  console.log('4primuse3');
+  console.log('4primuse3')
   resolve()
 }).then(function () {
-  console.log('7primuse4');
+  console.log('7primuse4')
 })
 
-console.log('5scritp end');
-
+console.log('5scritp end')
 
 // 第一遍正常运行
 // 打印 scritp start（1）
@@ -76,7 +75,6 @@ console.log('5scritp end');
 // 第三遍 执行 宏任务
 // setTimeout 打印 setTimeout（9）
 
-
 var outVar = '最外层变量'
 
 // 最外层函数
@@ -91,7 +89,6 @@ function outFun() {
   innerFun()
 }
 
-
 console.log(outVar)   // ==> 最外层变量
 
 outFun()              // ==> 内层变量
@@ -100,3 +97,54 @@ console.log(inVar)    // ==> inVar is not defined
 
 innerFun()            // ==> innerFun is not defined
 
+var a = 10
+
+function f1() {
+  var b = 20
+  
+  function f2() {
+    var c = 30
+    
+    console.log(a)  // 自由变量，顺着作用域链找
+    console.log(b)  // 自由变量，顺着作用域链找
+    console.log(c)  // 当前作用域变量
+  }
+  
+  f2()
+}
+
+f1()
+
+var x = 10
+
+function fn() {
+  console.log(x) // 10
+}
+
+function show(f) {
+  var x = 20
+  
+  (function () {
+    f() // 10, 而不是20
+  })()
+}
+
+show(fn)
+
+var a = 10
+
+function fn() {
+  var b = 20
+  
+  function bar() {
+    console.log(a + b)  // 30
+  }
+  
+  return bar
+}
+
+var x = fn() // x = bar
+
+b = 200
+
+x() // bar() ==> 30
