821bc5161892571f9ac627a905890f3ade0b4cc1
tangxue <11183504@qq.com>
2021-04-26 18:07:16 +0800

    update

--- docs/fe/js/index.js
+++ docs/fe/js/index.js
@@ -1,3 +1,29 @@
+
+
+function bubbleSort2(arr) {
+  console.time('改进后冒泡排序耗时')
+  var i = arr.length - 1  //初始时,最后位置保持不变
+  while (i > 0) {
+    var pos = 0 //每趟开始时,无记录交换
+    console.log('pos', pos)
+    console.log('i=====', i)
+    for (var j = 0; j < i; j++)
+      if (arr[j] > arr[j + 1]) {
+        pos = j //记录交换的位置
+        var tmp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = tmp
+      }
+    i = pos //为下一趟排序作准备
+    console.log('i---', i)
+  }
+  console.timeEnd('改进后冒泡排序耗时')
+  return arr
+}
+
+var arr = [3, 44, 38, 19, 50, 48]
+console.log(bubbleSort2(arr))//[2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50]
+
 class RemoveCommentsPlugin {
   apply(compiler) {
     compiler.hooks.emit.tap('RemoveCommentsPlugin', compilation => {
