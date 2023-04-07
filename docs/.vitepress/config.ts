62c17d400f27c026e3471f39872cc266e37af781
tangxve <11183504@qq.com>
2023-04-07 19:01:04 +0800

    feat: 更新 导航

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,25 +4,24 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: '知食',
   description: 'A VitePress Site',
+  lastUpdated: true,
+  cleanUrls: true,
+
+  /* markdown 配置 */
+  markdown: {
+    lineNumbers: true
+  },
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: '彩虹桥', link: '/bifrost' },
+      { text: '🌈 彩虹桥', link: '/bifrost' },
       { text: '日常笔记', link: '/a' },
       { text: '前端相关', link: '/b' },
       { text: 'Examples', link: '/markdown-examples' }
     ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
+    sidebar: {},
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
