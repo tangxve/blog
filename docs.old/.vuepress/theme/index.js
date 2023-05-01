f5a13bc0d3be401e4de8da81d6352504ba9df99f
唐雪 <11183504@qq.com>
2023-05-02 00:38:27 +0800

    feat: test

--- docs.old/.vuepress/theme/index.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-module.exports = {
-  extend: '@vuepress/theme-default',
-  // name: 'vuepress-theme-local',
-  // devTemplate: path.resolve(__dirname, 'templates/index.dev.html'),
-  // ssrTemplate: path.resolve(__dirname, 'templates/index.dev.html'),
-  // globalLayout: path.resolve(__dirname, 'layouts/Layout.vue'),
-};
