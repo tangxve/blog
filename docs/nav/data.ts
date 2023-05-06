74bc4cc8fae25801177cdc3e4fd33e51ba02d5f4
唐雪 <11183504@qq.com>
2023-05-06 15:29:40 +0800

    feat: update

--- docs/nav/data.ts
+++ docs/nav/data.ts
@@ -116,22 +116,10 @@ export const NAV_DATA: NavData[] = [
         link: 'https://www.nextjs.cn/'
       },
       {
-        icon: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
-        title: 'UmiJS',
-        desc: '插件化的企业级前端应用框架',
-        link: 'https://umijs.org'
-      },
-      {
         icon: 'https://gw.alipayobjects.com/zos/rmsportal/rlpTLlbMzTNYuZGGCVYM.png',
         title: 'Ant Design',
         desc: '一套企业级 UI 设计语言和 React 组件库',
         link: 'https://ant.design'
-      },
-      {
-        icon: 'https://gw.alipayobjects.com/zos/bmw-prod/69a27fcc-ce52-4f27-83f1-c44541e9b65d.svg',
-        title: 'Ant Design Mobile',
-        desc: '构建移动 WEB 应用程序的 React 组件库',
-        link: 'https://mobile.ant.design'
       },
       {
         icon: 'https://docs.pmnd.rs/apple-touch-icon.png',
@@ -146,28 +134,16 @@ export const NAV_DATA: NavData[] = [
         link: 'https://valtio.pmnd.rs'
       },
       {
-        icon: 'https://jotai.org/favicon.svg',
-        title: 'Jotai',
-        desc: 'primitive and flexible state management for React',
-        link: 'https://jotai.org'
-      },
-      {
-        icon: 'https://cn.redux.js.org/img/redux.svg',
-        title: 'Redux',
-        desc: 'JavaScript 应用的状态容器，提供可预测的状态管理',
-        link: 'https://cn.redux.js.org'
-      },
-      {
-        icon: 'https://zh.mobx.js.org/assets/mobx.png',
-        title: 'MobX',
-        desc: '一个小型、快速、可扩展的 React 状态管理解决方案',
-        link: 'https://zh.mobx.js.org'
-      },
-      {
         icon: 'https://ahooks.js.org/simple-logo.svg',
         title: 'ahooks',
         desc: '一套高质量可靠的 React Hooks 库',
         link: 'https://ahooks.js.org/zh-CN'
+      },
+      {
+        icon: 'https://create.t3.gg/images/t3-dark.svg',
+        title: 'create t3 app',
+        desc: '让你以最快的方式启动一个独立、全栈、类型安全 NextJS 应用。',
+        link: 'https://create.t3.gg/zh-hans/introduction'
       }
     ]
   },
