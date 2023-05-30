2624d4be0b4e0947885a448f76cb5341063b286c
唐雪 <11183504@qq.com>
2023-05-30 20:23:23 +0800

    feat: 添加最近使用（vue 操作数组真的脑残啊）

--- docs/nav/Nav.vue
+++ docs/nav/Nav.vue
@@ -1,20 +1,46 @@
 <script setup lang="ts">
+import { reactive, ref } from 'vue'
 import NavLink from './NavLink.vue'
 import NavLinks from './NavLinks.vue'
 import { NAV_DATA } from './data'
 import type { NavLinkType } from './type'
 
+const LOCAL_KEY = '__recentLinks__'
 
-// 最近使用历史
-const RECENTLY_USED: NavLinkType[] = []
+// 获取 localStorage 中的 recentLinks，如果不存在则返回空数组
+const recentLinks: NavLinkType[] = ref(JSON.parse(localStorage.getItem(LOCAL_KEY)) || [])
 
 const handleClick = (item: NavLinkType) => {
-  console.log('handleClick', item)
+  updateRecentLinks(item)
+}
+
+// 更新 recentLinks
+const updateRecentLinks = (item: NavLinkType) => {
+  const index = recentLinks.value.findIndex((link) => link.link === item.link)
+  if (index !== -1) {
+    return
+  }
+
+  recentLinks.value.unshift(item)
+
+  // 只保留最近的5个链接
+  recentLinks.value = [...recentLinks.value].slice(0, 5)
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(recentLinks.value))
 }
 
 </script>
 
 <template>
+  <NavLinks title="最近使用" :items="recentLinks">
+    <NavLink
+      v-for="{ icon, title, desc, link } in recentLinks"
+      :key="link"
+      :icon="icon"
+      :title="title"
+      :desc="desc"
+      :link="link"
+    />
+  </NavLinks>
   <NavLinks
     v-for="{title, items} in NAV_DATA"
     :title="title" :items="items">
