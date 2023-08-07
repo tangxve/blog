3d7e73d9112395d0f2426cb50bfba73e47702ea5
唐雪 <11183504@qq.com>
2023-08-07 19:03:32 +0800

    feat: update

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -10,6 +10,7 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
       text: '2022 年',
       collapsed: false,
       items: [
+        { text: '项目中依赖同一个库多个版本', link: '/notes/9' },
         { text: 'zsh 更新问题', link: '/notes/8' },
         { text: '富文本内容展示问题', link: '/notes/7' },
         { text: 'canvas 工具方法', link: '/notes/6' },
